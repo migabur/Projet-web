4e832958270c1775e095d6c2cb5f9496bdd19423
HS <seb.frkl@hotmail.fr>
2018-08-28 13:05:54 +0200

    9a ressemble tjrs a rien

--- Script.js
+++ Script.js
@@ -4,12 +4,25 @@ function createParagraph() {
   document.body.appendChild(para);
 }
 
-var randomNumber = Math.floor(Math.random()*100)+1
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+
+var guessCount = 1;
+var resetButton;
 
 var buttons = document.querySelectorAll('button');
 
 for(var i = 0; i < buttons.length ; i++) {
   buttons[i].addEventListener('click', createParagraph);
+
+  function checkGuess(){
+  	alert('hshgdqs')
+  }
 }
 
