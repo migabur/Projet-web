1c8f99aee0b172847e10220bccbf25205d2b7a32
migabur <37173118+migabur@users.noreply.github.com>
2018-09-03 09:29:12 +0200

    trying to get back what seb did

--- serveur.js
+++ serveur.js
@@ -1,8 +1,17 @@
-var http = require('http')
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res){
+	var params = querystring.parse(url.parse(req.url).query);
+	var page = url.parse(req.url).pathname;
+	console.log(page);
 	res.writeHead(200);
-	res.end('Hi everybody, how do ya da di dou ka da badoo');
-
+	res.write('Hi everybody, how do ya da di dou ka da badoo');
+	if ('prenom' in params && 'nom' in params) {
+		res.write('Your name is' + params['prenom'] + ' ' + params['nom']);
+	}
+	res.end();
 });
 server.listen(8080);
+
