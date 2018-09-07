d84c5797955977b1097752f876553bc183ba093f
SH <seb.frkl@hotmail.fr>
2018-09-07 19:12:24 +0200

    Final commit

--- public/js/testScript.js
+++ public/js/testScript.js
@@ -1,273 +1,237 @@
 var app = new Vue({
-	el:'#app',
+    el: '#app',
 
-	data:{
-		titlePage:'Mario Wiki',
-		mainText:'Welcome to the website MarioWiki. This wiki aims for the community to share it\'s tips and tricks between them.',
-		GoodGuys:[],
-		BadGuys:[],
-		Items:[],
-		Maps:[],
-		cat:'',
-		userName:'',
-		image:'assets/cover_mario.jpg',
-		imagealt:'assets/cover_mario.jpg',
-		alertMessage:'default message',
-		displayAlert:false,
-		displayLogin:true,
-		category:[],
-		loggedin:false,
-		onHome:true,
-
-		homeText:'Welcome to the website MarioWiki. This wiki aims for the community to share it\'s tips and tricks between them.',
-		homeImage:'assets/cover_mario.jpg',
-		homeTitle:'Mario Wiki'
-	},
-	mounted:function(){
-		this.getJSONAll()
-
-		/*.then(function(data){
-			this.GoodGuys=data["Good Guys"]
-			console.log(GoodGuys)
-		})*/
-
-	},
-	methods: {
-
-		getJSONAll: function(){
-
-			var _this = this
-			var jsonFile = new Promise((resolve)=>{getJSONAsString(resolve)}).then(function(value){
-			var js = JSON.parse(value)
-			for  (var cat in js)
-			{
-				if(js.hasOwnProperty(cat)){
-					console.log(cat)
-					_this.category.push(cat)
-				}
-			}
-			console.log(js)
-			for  (var gG in js["Good Guys"])
-			{
-				if(js["Good Guys"].hasOwnProperty(gG)){
-					console.log(gG)
-					_this.GoodGuys.push(gG)
-				}
-			}
-			for  (var bG in js["Bad Guys"])
-			{
-									console.log(bG)
+    data: {
+        titlePage: 'Mario Wiki',
+        mainText: 'Welcome to the website MarioWiki. This wiki aims for the community to share it\'s tips and tricks between them.',
+        GoodGuys: [],
+        BadGuys: [],
+        Items: [],
+        Maps: [],
+        cat: '',
+        userName: '',
+        image: 'assets/cover_mario.jpg',
+        imagealt: 'assets/cover_mario.jpg',
+        alertMessage: 'default message',
+        displayAlert: false,
+        displayLogin: true,
+        category: [],
+        loggedin: false,
+        onHome: true,
+        carousel: false,
 
 
-					console.log(bG)
-					console.log('BADGUYS')
-					_this.BadGuys.push(bG)
-				
-			}
-			for  (var i in js["Items"])
-			{
-									console.log(i)
+        Tips: [],
+
+        homeText: 'Welcome to the website MarioWiki. This wiki aims for the community to share it\'s tips and tricks between them.',
+        homeImage: 'assets/cover_mario.jpg',
+        homeTitle: 'Mario Wiki'
+    },
+    mounted: function() {
+        this.getJSONAll()
+
+    },
+    methods: {
+
+        getJSONAll: function() {
+
+            var _this = this
+            var jsonFile = new Promise((resolve) => { getJSONAsString(resolve) }).then(function(value) {
+                var js = JSON.parse(value)
+                for (var cat in js) {
+                    if (js.hasOwnProperty(cat)) {
+                        _this.category.push(cat)
+                    }
+                }
+                for (var gG in js["Good Guys"]) {
+                    if (js["Good Guys"].hasOwnProperty(gG)) {
+                        _this.GoodGuys.push(gG)
+                    }
+                }
+                for (var bG in js["Bad Guys"]) {
+
+                    _this.BadGuys.push(bG)
+
+                }
+                for (var i in js["Items"]) {
+
+                    _this.Items.push(i)
+
+                }
+                for (var map in js["Maps"]) {
+
+                    _this.Maps.push(map)
+
+                }
+            })
+        },
 
 
-					console.log(i)
-					console.log('i')
-					_this.Items.push(i)
-				
-			}
-			for  (var map in js["Maps"])
-			{
-									console.log(map)
+        createPage: function(submitEvent) {
+            var cate = submitEvent.target.elements.cat.value
+            var name = submitEvent.target.elements.pageName.value
+            var text = submitEvent.target.elements.pageText.value
+            var image = submitEvent.target.elements.image.value
+
+            this.$http.post('/createPage', { cat: cate, titlePage: name, textEdit: text, image: image }).then(function(response) {
+                this.GoodGuys = []
+                this.BadGuys = []
+                this.category = []
+                this.Items = []
+                this.Maps = []
+                this.getJSONAll();
+                this.alertMessage = response.body;
+                this.displayAlert = true;
+            });
+        },
+
+        changePage: function(id) {
+            /*var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/editpage', true);
+            xhr.send(null);
+            xhr.onreadystatechange = function(){
+
+            }},*/
+            this.onHome = false
+            var _this = this;
+            var xhr = new XMLHttpRequest();
+
+            xhr.open('GET', '/changepage/' + id, true);
+
+            xhr.send(null)
+            xhr.onreadystatechange = function() {
+                // XMLHttpRequest.DONE === 4
+
+                if (this.readyState === XMLHttpRequest.DONE) {
+                    if (this.status === 200) {
+                        var js = JSON.parse(this.responseText)
+                        var tempString = id.split('/')
+                        tempString = tempString[1]
+                        _this.cat = id.substring(0, id.indexOf('/'))
+                        _this.titlePage = tempString
+                        _this.image = js.image
+                        _this.mainText = js["Text"]; //JSON.parse(this.responseText).EmployeeId;
+                        _this.imageTips = [];
+                        _this.carousel = false
+                        for (var tip in js.tips) {
+                            var obj = { image: js.tips[tip].image, text: js.tips[tip].text }
+                            _this.Tips.push(obj)
+                            _this.carousel = true
+                        }
+
+                    } else {
+                        console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+                    }
+                }
 
 
-					console.log(map)
-					console.log('m')
-					_this.Maps.push(map)
-				
-			}			console.log('BADGUYS'+js["Bad Guys"].Gamba)
-		})
-		},
+            }
 
 
-		createPage:function(submitEvent){
-			var cate=submitEvent.target.elements.cat.value
-			var name=submitEvent.target.elements.pageName.value
-			var text=submitEvent.target.elements.pageText.value
-			var image=submitEvent.target.elements.image.value
-			console.log('cat   ' +cate+text)
-			this.$http.post('/createPage', {cat: cate, titlePage: name, textEdit:text,image:image}).then(function(response){
-				 this.GoodGuys=[]
-				 this.BadGuys=[]
-				 this.category=[]
-				 this.Items=[]
-				 this.Maps=[]
- 		this.getJSONAll();
-				 										this.alertMessage=response.body;
-				this.displayAlert=true;
-			});
-		},
-		
-		changePage:function(id){
-			/*var xhr = new XMLHttpRequest();
-			xhr.open('GET', '/editpage', true);
-			xhr.send(null);
-			xhr.onreadystatechange = function(){
+        },
 
-			}},*/
-			this.onHome=false
-			console.log(id)
-			var _this = this;
-			var xhr = new XMLHttpRequest();
-			console.log('/changepage/'+id)
+        createAccount: function(submitEvent) {
+            var userName = submitEvent.target.elements.login.value
+            var password = submitEvent.target.elements.password.value
+            var password2 = submitEvent.target.elements.password2.value
+            this.$http.post('/createUser', { usr: userName, pwd1: password, pwd2: password2 }).then(function(response) {
+                this.alertMessage = response.body;
+                this.displayAlert = true;
 
-			xhr.open('GET', '/changepage/'+id, true);
+            })
+        },
 
-			xhr.send(null)
-			xhr.onreadystatechange = function() {
-    		// XMLHttpRequest.DONE === 4
-			
-    		if (this.readyState === XMLHttpRequest.DONE) {
-    			if (this.status === 200) {
-    				console.log(xhr.getResponseHeader('Content-Type'))
-    				var js = JSON.parse(this.responseText)
-    				  console.log(js)
-    				  var tempString = id.split('/')
-    				  console.log("temp string "+id)
-    				  tempString=tempString[1]
-    				  _this.cat = id.substring(0,id.indexOf('/'))
-    				  console.log("cat     "+_this.cat)
-    				_this.titlePage=tempString
-    				_this.image=js.image
-    				console.log('titlePage')
-    				console.log('image                                       IMAGE'+js.image)
-    				_this.mainText=js["Text"];//JSON.parse(this.responseText).EmployeeId;
-    			} else {
-    				console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
-    			}
-    		}
+        logout: function() {
+            this.displayLogin = !(this.displayLogin)
+            this.$http.post('/logout', { usr: this.userName }).then(function(response) {
+                this.alertMessage = response.body;
+                this.displayAlert = true;
+                this.userName = ""
+                this.loggedin = false
 
+            })
+        },
 
-}
+        login: function(submitEvent) {
+            var usr = submitEvent.target.elements.user.value
+            var pwd = submitEvent.target.elements.password.value
+            this.$http.post('/login', { password: pwd, login: usr }).then(function(response) {
+                this.alertMessage = response.body;
+                this.displayAlert = true;
+                if (response.bodyText === "Logged in") {
+                    this.userName = usr
+                    this.displayLogin = !(this.displayLogin)
+                    console.log('LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOG INNNNNNNNNNNNN')
+                    this.loggedin = true
 
+                } else { console.log('NOTTTTTTTTTTTTTTTT LOGGGGGGGGGGG INNNNNNNNNNNN') }
 
-},
+            });
+        },
 
-		createAccount:function(submitEvent){
-			var userName=submitEvent.target.elements.login.value
-			var password=submitEvent.target.elements.password.value
-			var password2=submitEvent.target.elements.password2.value
-			this.$http.post('/createUser', {usr:userName, pwd1:password,pwd2:password2}).then(function(response){
-								this.alertMessage=response.body;
-				this.displayAlert=true;
+        editPage: function() {
+            console.log('cat   ' + this.cat + this.mainText)
+            this.$http.post('/editPage', { cat: this.cat, titlePage: this.titlePage, textEdit: this.mainText, image: this.image }).then(function(response) {
+                this.alertMessage = response.body;
+                this.displayAlert = true;
+            });
+        },
 
-			})
-		},
+        deletePage: function() {
+            this.$http.post('/deletePage', { cat: this.cat, titlePage: this.titlePage }).then(function(response) {
+                this.GoodGuys = []
+                this.BadGuys = []
+                this.category = []
+                this.Items = []
+                this.Maps = []
 
-		logout:function(){
-			this.displayLogin=!(this.displayLogin)
-			this.$http.post('/logout', {usr:this.userName}).then(function(response){
-				this.alertMessage=response.body;
-				this.displayAlert=true;
-				this.userName=""
-				this.loggedin=false
-				console.log("loggedin"+this.loggedin)
+                this.getJSONAll();
+                this.alertMessage = response.body;
+                this.displayAlert = true;
+            })
+        },
 
-			})
-		},
+        getJsonObject: function(cb) {
+            var request = new XMLHttpRequest();
 
-		login:function(submitEvent){
-			var usr = submitEvent.target.elements.user.value
-			var pwd = submitEvent.target.elements.password.value
-			this.$http.post('/login', {password: pwd, login: usr}).then(function(response) {
-				this.alertMessage=response.body;
-				this.displayAlert=true;
-				if(response.bodyText==="Logged in"){
-					this.userName=usr
-					this.displayLogin=!(this.displayLogin)
-					console.log('LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOG INNNNNNNNNNNNN')
-									this.loggedin=true
+            request.open('GET', 'js/test.json', true);
+            request.send(null);
+            request.onreadystatechange = function() {
+                if (request.readyState === 4 && request.status === 200) {
+                    var type = request.getResponseHeader('Content-Type');
 
-				}
-				else{console.log('NOTTTTTTTTTTTTTTTT LOGGGGGGGGGGG INNNNNNNNNNNN')}
+                    try {
 
-			});
-		},
+                        cb(request.responseText);
+                    } catch (err) {
+                        cb(err);
+                    }
+                }
+            }
 
-		editPage:function(){
-			console.log('cat   ' +this.cat+this.mainText)
-			this.$http.post('/editPage', {cat: this.cat, titlePage: this.titlePage, textEdit:this.mainText,image:this.image}).then(function(response){
-				 										this.alertMessage=response.body;
-				this.displayAlert=true;
-			});
-		},
-		/*dostuff:function(){
-		var _this = this;
-		this.getJsonObject(function(object){
-     	//Do what you want with the object here
+        }
 
-     	console.log(object.EmployeeId)
-     	_this.mainText=object.EmployeeId
-		console.log(_this.mainText)
-     	console.log(object);
-     });
-
- },*/
- deletePage:function(){
- 	this.$http.post('/deletePage', {cat : this.cat, titlePage:this.titlePage}).then(function(response){
- 		this.GoodGuys=[]
- 		this.BadGuys=[]
- 		this.category=[]
- 		this.Items=[]
- 						 this.Maps=[]
-
- 		this.getJSONAll();
- 										this.alertMessage=response.body;
-				this.displayAlert=true;
- 	})
- },
-
- getJsonObject: function(cb){
- 	var request = new XMLHttpRequest();
-
- 	request.open('GET', 'js/test.json', true);
- 	request.send(null);
- 	request.onreadystatechange = function () {
- 		console.log(request.status)
- 		if (request.readyState === 4 && request.status === 200) {
- 			var type = request.getResponseHeader('Content-Type');
-
- 			try {
-
- 				cb(request.responseText);
- 			}catch(err) {
- 				cb(err);
- 			}
- 		}
- 	}
-
- }
-
-}
+    }
 
 })
 
 
 
-async function getJSONAsString(resolve){
-	var xhr = new XMLHttpRequest();
+async function getJSONAsString(resolve) {
+    var xhr = new XMLHttpRequest();
 
-	xhr.onreadystatechange = function(){
-	if (this.readyState === XMLHttpRequest.DONE) {
-    			if (this.status === 200) {
-    				console.log('type '+xhr.getResponseHeader('Content-Type'))
-    				console.log('this.responseText  '+this.responseText)
-    				console.log(typeof(this.responseText))
-    				setTimeout(() => resolve (this.responseText), 1)
-    				resolve(this.responseText);
-    			} else {
-    				console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
-    			}
-    		}
-	}
-		xhr.open('GET', '/getJSON', true);
-	xhr.send(null)
+    xhr.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                console.log('type ' + xhr.getResponseHeader('Content-Type'))
+
+                setTimeout(() => resolve(this.responseText), 1)
+                resolve(this.responseText);
+            } else {
+                console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+            }
+        }
+    }
+    xhr.open('GET', '/getJSON', true);
+    xhr.send(null)
 }
--- server.js
+++ server.js
@@ -7,197 +7,162 @@ const fs = require('fs')
 
 const app = express()
 app.use(bodyParser.json())
- app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-var users=[]
+var users = []
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/views/layouts/main.html'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/layouts/main.html'))
 })
 
 const port = 3000
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-  console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`)
 })
 
-/*app.get('/', (request, response) => {
-  response.render('main', {
-  })        
-})                 a quoi ca sert ca?*/ 
-
-
-
-/*const users = []
-app.post('/users', function (req, res){
-  const user = req.body
-  users.push({
-    name: user.name,
-    age: user.age
-  })
-  res.send('successfully registered')
-})*/
 
 app.post('/createUser', function(req, res) {
-            fs.readFile('src/identification.json', function(err, data) {
-                var file = JSON.parse(data)
-                if (req.body.pwd1 != req.body.pwd2) {
-                    res.send("Password not matching")
-                    return
+    fs.readFile('src/identification.json', function(err, data) {
+        var file = JSON.parse(data)
+        if (req.body.pwd1 != req.body.pwd2) {
+            res.send("Password not matching")
+            return
 
-                } else if (!checkUserInJSONFile(file, req.body.usr)) {
-                    res.send("User already exist")
-                    return
-                } else {
+        } else if (!checkUserInJSONFile(file, req.body.usr)) {
+            res.send("User already exist")
+            return
+        } else {
 
-                    var login = req.body.usr;
+            var login = req.body.usr;
 
-                  console.log('LOGIN'+login)
-                                      var password = req.body.pwd1;
-                    var newUser = {
-                        "user": login,
-                        "password": password
-                    }
-                    file.push(newUser)
+            var password = req.body.pwd1;
+            var newUser = {
+                "user": login,
+                "password": password
+            }
+            file.push(newUser)
 
-                    fs.writeFileSync('src/identification.json', JSON.stringify(file));
-                    res.send("Account successfully created")
-                }
-            })
+            fs.writeFileSync('src/identification.json', JSON.stringify(file));
+            res.send("Account successfully created")
         }
-        )
+    })
+})
 
-function checkUserInJSONFile(file, userName){
-  for (user in file){
-    console.log(file[user])
-    if (file[user].user===userName){
-      return false
+function checkUserInJSONFile(file, userName) {
+    for (user in file) {
+        if (file[user].user === userName) {
+            return false
+        }
     }
-  }
-  return true
+    return true
 }
 
 
-app.post('/login', function(req,res){
-  fs.readFile('src/identification.json', function(err, data){
-  var userList = JSON.parse(data)
-  var login = req.body.login;
-  var password = req.body.password;
+app.post('/login', function(req, res) {
+    fs.readFile('src/identification.json', function(err, data) {
+        var userList = JSON.parse(data)
+        var login = req.body.login;
+        var password = req.body.password;
 
-  for (var i = userList.length - 1; i >= 0; i--) {
-    console.log(req.body.login)
-    console.log(req.body.password)
-    if(userList[i].user===login && userList[i].password===password){
-      users.push(login)
-      res.send("Logged in")
-      return
-    }
-  }
-  res.send("Invalid creditential")
-})
-})
+        for (var i = userList.length - 1; i >= 0; i--) {
 
-app.post('/logout', function(req, res){
-  users.splice(users.indexOf(req.body.user),1)
-  res.send('Logged out')
-
+            if (userList[i].user === login && userList[i].password === password) {
+                users.push(login)
+                res.send("Logged in")
+                return
+            }
+        }
+        res.send("Invalid creditential")
+    })
 })
 
-app.get('/changepage/:cat/:name', function(req, res){
+app.post('/logout', function(req, res) {
+    users.splice(users.indexOf(req.body.user), 1)
+    res.send('Logged out')
 
-  console.log("name "+req.params.name)
+})
 
-  console.log("cat " +req.params.cat)
-  fs.readFile('src/test.json', function(err, data){
-    if(err){
-      return console.log(('error ', err))
-    }
+app.get('/changepage/:cat/:name', function(req, res) {
 
-    var js = JSON.parse(data);
-    console.log(js)
-    console.log('to return+ '+js[req.params.cat][req.params.name].image)
-    res.send(js[req.params.cat][req.params.name]);
-  })
+
+    fs.readFile('src/test.json', function(err, data) {
+        if (err) {
+            return console.log(('error ', err))
+        }
+
+        var js = JSON.parse(data);
+        res.send(js[req.params.cat][req.params.name]);
+    })
 });
 
- app.post('/editPage', function(req, res){
-  console.log('body'+req.body)
-   editNameJSON(req.body.cat, req.body.titlePage, req.body.textEdit, req.body.image);
-   res.send("Edited page")
-  //removeNameJSON('Good Guys', 'Mario')
+app.post('/editPage', function(req, res) {
+    editNameJSON(req.body.cat, req.body.titlePage, req.body.textEdit, req.body.image);
+    res.send("Edited page")
+    //removeNameJSON('Good Guys', 'Mario')
 })
 
-app.post('/createPage',function(req, res){
-  var message = addPageJSON(req.body.cat, req.body.titlePage, req.body.textEdit, req.body.image);
-  res.send("Created page")
+app.post('/createPage', function(req, res) {
+    var message = addPageJSON(req.body.cat, req.body.titlePage, req.body.textEdit, req.body.image);
+    res.send("Created page")
 
 })
 
-app.post('/deletepage', function(req, res){
-  removePageJSON(req.body.cat, req.body.titlePage)
-  res.send("Page deleted")
+app.post('/deletepage', function(req, res) {
+    removePageJSON(req.body.cat, req.body.titlePage)
+    res.send("Page deleted")
 })
 
-app.get('/getJSON', function(req, res){
-    fs.readFile('src/test.json', function(err, data){
-    if(err){
-      return console.log(('error ', err))
-    }
-    var json =JSON.parse(data)
-    console.log(json)
-    //res.setHeader('Content-Type', 'application/json')
+app.get('/getJSON', function(req, res) {
+    fs.readFile('src/test.json', function(err, data) {
+        if (err) {
+            return console.log(('error ', err))
+        }
+        var json = JSON.parse(data)
+        console.log(json)
+        //res.setHeader('Content-Type', 'application/json')
 
-    res.send(json);
-  })
+        res.send(json);
+    })
 })
 
-function editNameJSON(cat, name, mainText, mainImage){
-              console.log('cat' +cat)
+function editNameJSON(cat, name, mainText, mainImage) {
+    console.log('cat' + cat)
 
-  fs.readFile('src/test.json', function(err, data){
-    var js = JSON.parse(data)
-        console.log('js   '+js)
-            console.log('cat' +cat)
-    console.log('name'+name)
-    js[cat][name].Text=mainText;
-    js[cat][name].image=mainImage
-    console.log('after  '+js[cat])
-
-   console.log('after '+ JSON.stringify(js))
-    fs.writeFileSync('src/test.json', JSON.stringify(js));
-
-  })
-}
-
-function addPageJSON(cat, name, mainText, mainImage){
-  fs.readFile('src/test.json', function(err, data){
+    fs.readFile('src/test.json', function(err, data) {
         var js = JSON.parse(data)
-    var content  = {Text:mainText,image:mainImage};
 
-    console.log('parsed '+content)
+        js[cat][name].Text = mainText;
+        js[cat][name].image = mainImage
 
-        console.log('js   '+js)
-    console.log('cat' +cat)
-   console.log('name'+name)
-    js[cat][name]=content;
-    console.log('after  '+js[cat])
 
-    console.log('after '+ JSON.stringify(js))
-    fs.writeFileSync('src/test.json', JSON.stringify(js));
+        fs.writeFileSync('src/test.json', JSON.stringify(js));
 
-  })
+    })
 }
 
-function removePageJSON(cat, name){
-  fs.readFile('src/test.json', function(err, data){
-    console.log("ça marche")
-    var js = JSON.parse(data)
-    delete js[cat][name]
-    fs.writeFileSync('src/test.json', JSON.stringify(js));
-  }) 
+function addPageJSON(cat, name, mainText, mainImage) {
+    fs.readFile('src/test.json', function(err, data) {
+        var js = JSON.parse(data)
+        var content = { Text: mainText, image: mainImage };
+
+
+        js[cat][name] = content;
+
+        fs.writeFileSync('src/test.json', JSON.stringify(js));
+
+    })
+}
+
+function removePageJSON(cat, name) {
+    fs.readFile('src/test.json', function(err, data) {
+        var js = JSON.parse(data)
+        delete js[cat][name]
+        fs.writeFileSync('src/test.json', JSON.stringify(js));
+    })
 }
